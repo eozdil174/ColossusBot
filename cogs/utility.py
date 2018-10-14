50e1fe405c82c0e5efe7ff6ace73692f815c3edd
Eren OZDIL <eozdil174@gmail.com>
2018-10-14 22:05:34 +0300

    Added 8ball function

--- cogs/utility.py
+++ cogs/utility.py
@@ -7,6 +7,7 @@ import discord
 from discord.utils import get
 from discord.ext import commands
 from datetime import datetime
+import random
 
 class Utility(object):
 
@@ -39,6 +40,13 @@ class Utility(object):
 
         else:       #For every other user
             await self.bot.say("I don't think so")
+
+    @commands.command(name = "8ball", pass_context=True)
+    async def _8ball(self, message):
+
+        answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.","Very doubtful."]
+        await self.bot.say(answers[random.randint(0,len(answers))])
+
 
     @commands.command(pass_context=True)
     async def fixCustomerRole(self, message):
